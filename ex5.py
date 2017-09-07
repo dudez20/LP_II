2c403f43c0756da853c3277a80ae234912ff938e
dudez20 <30813647+dudez20@users.noreply.github.com>
2017-09-06 22:21:47 -0300

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,20 @@
+y = 0
+nom={}
+email={}
+tel={}
+ende={}
+while x=4 :
+    x=int(input("Digite 1 para inserir, 2 remover, 3 pesquisar, 4 sair"))
+    if x = 1:
+        nom[y] = input("Digite um nome: ")
+        email[y] = input("Digite um e-mail: ")
+        tel[y] = input("Digite um número de telefone: ")
+        ende[y] = input("Digite um endereço: ")
+        
+    elif x = 2:
+
+    elif x = 3:
+        
+y=+1
+
+print("nom, email, tel, ende")
